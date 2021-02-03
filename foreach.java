a7c4e0e61b63f27bd9f808ecd5aaf8d822c573bf
ghanshyamcode <ghanshyamv2kv@gmial.com>
2021-02-03 20:20:47 +0530

    Program to learn use of for_each

---
+++ foreach.java
@@ -0,0 +1,13 @@
+//use a for-each style for loop.
+public class foreach {
+    public static void main(String args[]){
+        int nums[]={1,2,3,4,5,6,7,8,9,10};
+        int sum=0;
+    
+        for(int x : nums){
+            System.out.println("value is: "+x);
+            sum +=x;
+    }
+   System.out.println("Summation: "+sum);
+}
+}
